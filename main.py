1c660d51b65abdfbb48405aa6bac46679ef5a632
serv0id <113516593+serv0id@users.noreply.github.com>
2025-06-25 17:28:31 +0530

    added skeleton main.py

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
+from io import BytesIO
 from loguru import logger
 from time import time
+from utils.unpacker import BitUnpacker
+import click
 
 
 class OPANQr(object):
-    def __init__(self):
-        self.output_dir = f"output/{time()}"
+    def __init__(self, string):
+        self.output_dir: str = f"output/{time()}"
+        self.photo: bytes
+        self.name: str
+        self.pan_number: str
+        self.scanned_string: str = string
+        self.unpacked_string: bytes = self.unpack()
+
+    def unpack(self) -> bytes:
+        stream = BytesIO()
+        unpacker = BitUnpacker(stream)
+
+        sliced_bytes = [self.scanned_string[i:i + 4] for i in range(0, len(self.scanned_string), 4)]
+        for sliced in sliced_bytes:
+            unpacker.bit_unpack(int(sliced), 13)
+
+        stream.seek(0)
+        return stream.read()
 
 
-def main() -> None:
-    pass
+@click.command()
+@click.option("--string", help="The scanned QR Code string as-is")
+def main(string) -> None:
+    opanqr = OPANQr(string)
+    print(opanqr.unpacked_string)
 
 
 if __name__ == "__main__":
---
+++ utils/parser.py
@@ -0,0 +1,7 @@
+import construct
+from constants.structs import PAN_OUTER_BLOCK_STRUCT
+
+
+class Parser(object):
+    def __init__(self):
+        pass
