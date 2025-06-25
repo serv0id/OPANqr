8f83c4ad0cc5f3f7d7d50e398e94ba25720aa5e3
serv0id <113516593+serv0id@users.noreply.github.com>
2025-06-25 17:07:57 +0530

    added character sets

--- constants/values.py
+++ constants/values.py
@@ -1,7 +1,13 @@
 CHARACTER_SETS = {
-    "Numeric_1": "",
-    "Numeric_2": "",
-    "Text": "",
-
+    "Numeric_1": "0123456789+-.%/*",
+    "Numeric_2": "0123456789-.%<>/",
+    "Text": None,
+    "AlphaNumericUpperCase": r"01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ ~!@#$%^&*()+-={[}]\\;:/?<.>",
+    "AlphaNumericLowerCase": r"01234567890abcdefghijklmnopqrstuvwxyz ~!@#$%^&*()+-={[}]\\;:/?<.>",
+    "AlphaNumeric": r"01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ~!@#$%^&*()+-={[}]\\;:/?<.>\',|`",
+    "AlphabetsUpperCase": r"ABCDEFGHIJKLMNOPQRSTUVWXYZ .\\-/\'",
+    "AlphabetsLowerCase": r"abcdefghijklmnopqrstuvwxyz .\\-/\'",
+    "Alphabets": r"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz .,-@\'*!|?_=",
+    "HexaDecimal": "0123456789ABCDEF"
 }
 
